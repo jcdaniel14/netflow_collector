8d11344dbde207755883ef01000486e63e06bcf4
Gustavo Santiago <gsantiago@telconet.ec>
2018-10-17 11:38:58 -0500

    Proxy Timelion APM and Monitoring

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,16 @@ var opts = {
 
 //Get HomePage 
 router.get('/',ensureAuthenticated,function(req,res){
-    res.render('index');
+    res.redirect('/app/kibana');
 });
 //Get Kibana
 router.get('/app/kibana',ensureAuthenticated,proxy(opts));
+//Get Timelion
+router.get('/app/timelion',ensureAuthenticated,proxy(opts));
+//Get APM
+router.get('/app/apm',ensureAuthenticated,proxy(opts));
+//Get Monitoring
+router.get('/app/monitoring',ensureAuthenticated,proxy(opts));
 
 function ensureAuthenticated(req,res,next){
     if(req.isAuthenticated())
